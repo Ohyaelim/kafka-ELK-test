a5e461aead2f0cdd5271c9b8fdfceccfacf8778f
ohyaelim <yaelim1122@shinsegae.com>
2022-11-18 15:55:01 +0900

    first commit

---
+++ src/main/java/com/kafkaelk/oyltest/OyltestApplication.java
@@ -0,0 +1,13 @@
+package com.kafkaelk.oyltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OyltestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OyltestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafkaelk/oyltest/OyltestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaelk.oyltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OyltestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
